533a567569cea3c39573cd4b03372fc81b0e6807
Tatiana <tatiana_ivanova89@mail.ru>
2022-04-16 17:35:50 +0700

    sprint_4 add tabs

--- src/script/script.js
+++ src/script/script.js
@@ -38,7 +38,6 @@ $('.modal').mouseup(function(e) {
 });
 
 /*  Фото в модальном окне  */
-
 $(document).ready(function(){
     $('.gallery__slider-item').click(function(e)
     {
@@ -51,18 +50,7 @@ $(document).ready(function(){
     });
 });
 
-/*  табы из секции услуги и цены  */
-
-// $(document).ready(function(){
-    // $('.navigation__item a').click(function(e)
-    // {
-    // var itemList = document.getElementsByClassName('section__list');
-    // console.log(itemList);
-    // });
-// });
-
 /*  данные из формы  */
-
 function serializeForm(formNode) {
     const { elements } = formNode
     const data = Array.from(elements)
@@ -81,4 +69,64 @@ function handleFormSubmit (event) {
 
 const applicantForm = document.getElementById('form')
 applicantForm.addEventListener('submit', handleFormSubmit)
+
+/*  табы из секции услуги и цены  */
+class TabItem {
+    constructor(link, content) {
+        this.link = link;
+        this.content = content;
+    }
+
+    onClick(callback) {
+        this.link.addEventListener('click', () => callback());
+    }
+
+    activate () {
+        this._toggle(true);
+    }
+
+    deactivate() {
+        this._toggle(false);
+    }
+
+    _toggle(activate) {
+        this.link.classList.toggle('active', activate);
+        this.content.classList.toggle('active', activate);
+    }
+}
+
+class TabsManager {
+    constructor(tabsElem) {
+        this.tabs = [];
+        this.activeTab = null;
+        this.init(tabsElem);
+        this.activateTab(this.tabs[0]);
+    }
+
+    init(tabsElem) {
+        const links = tabsElem.querySelectorAll('.tabs__links li');
+        const contents = tabsElem.querySelectorAll('.tabs__item');
+        
+        for (let i = 0; i < links.length; i++) {
+            const tab = new TabItem(links[i], contents[i]);
+            console.log(tab);
+            this.tabs.push(tab);
+
+            tab.onClick(() => this.activateTab(tab));
+        }
+    }
+    activateTab(tab) {
+        if (this.activeTab) {
+            this.activeTab.deactivate();
+        }
+        this.activeTab = tab;
+        this.activeTab.activate();
+    }
+}
+
+window.onload = function() {
+    const tabsElem = document.getElementById('myTabs');
+    new TabsManager(tabsElem);
+}
+
 
